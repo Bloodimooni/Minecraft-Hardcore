645553b47f7e221b28c3b0a96e9b7e7ace82753c
Joshua <39704584+Bloodimooni@users.noreply.github.com>
2023-06-01 21:42:18 +0200

    Update hardcore.py
    Fixed a bunch of stuff and added some features

--- server/hardcore.py
+++ server/hardcore.py
@@ -26,14 +26,12 @@ RESET = "\033[0m"
 FIRSTRUN = False
 
 operating_system = platform.system()
-usernames = []
+
 allowed_death_entities = [
-    "Villager",
-    "Cat",
-    "Wolf",
-    "Dog",
-    "Horse",
-    "Parrot"
+    "Named entity class",
+    "entity class",
+    "class_1646",
+    "*"
 ]
 
 death_messages = [
@@ -41,18 +39,26 @@ death_messages = [
     "drowned",
     "death",
     "experienced kinetic energy",
+    "intentional game design",
     "blew up",
+    "pummeled",
     "blown up",
     "killed",
     "hit the ground too hard",
     "fell",
+    "left the confines of this world",
+    "squished",
     "suffocated",
+    "was burnt",
+    "cactus",
     "was slain",
+    "was shot",
     "burned to death",
     "tried to swim in lava",
     "got melted by a blaze",
     "failed to escape the Nether",
     "fell out of the world",
+    "withered away",
     "discovered the void",
     "discovered the floor was lava",
     "was doomed by the Wither",
@@ -60,13 +66,32 @@ death_messages = [
     "got caught in a trap",
     "was pricked to death",
     "got stung by a bee",
+    "was stung to death",
+    "stung",
+    "doomed to fall",
     "starved to death",
     "was doomed by a witch",
     "fell into a ravine",
-    "was shot by a skeleton",
+    "was fireballed",
+    "was shot by a Skeleton",
     "was blown off a cliff",
     "got suffocated in a wall",
-    "was slain by a zombie"
+    "was slain by a zombie",
+    "was struck by lightning",
+    "impaled",
+    "squashed",
+    "went up in flames",
+    "flames",
+    "didn't want to live",
+    "skewered",
+    "walked into fire",
+    "went off with a bang",
+    "walked into the danger zone",
+    "was killed by magic",
+    "froze to death",
+    "was fireballed",
+    "obliterated",
+    "starved"
 ]
 
 # RIP Manu, he died for testing purposes
@@ -77,15 +102,10 @@ def enable_hardcore_mode():
     hardcore_key = "hardcore=false"
     hardcore_new = "hardcore=true"
 
-    # Open the input file and create a temporary file for writing
     with open(file_path, "r") as input_file, open(temp_file_path, "w") as temp_file:
-        # Read each line from the input file
         for line in input_file:
-            # Check if the line contains the hardcore setting
             if line.strip() == hardcore_key:
-                # Replace the line with the new value
                 line = hardcore_new + "\n"
-            # Write the modified or unchanged line to the temporary file
             temp_file.write(line)
 
     # Replace the original file with the temporary file
@@ -124,6 +144,57 @@ def check_and_create_run_file(operating_system):
             run_file.write(run_file_content)
         print(GREEN + f"[+]\t{run_file_path} created successfully." + RESET)         
 
+def sum_of_deaths(deaths : dict):
+    attempt_number = 1
+    for i in deaths:
+        attempt_number += deaths[i]
+    return attempt_number 
+
+
+def read_stats_from_file():
+    stats = {}
+    try:
+        with open("stats","r") as file:
+            out = file.read().replace("{","").replace("}","").replace("'","").split(",")
+            for item in out:
+                item = item.strip()
+                key, value = item.split(":")
+                key = key.strip()
+                value = int(value.strip())
+                stats[key] = value
+
+            return stats
+
+    except FileNotFoundError:
+        return stats
+
+def save_stats_to_file(stats):
+    with open("stats","w") as file:
+        file.write(str(stats))
+
+def player_died(player):
+    print(RED + "[-]\tA player died" + RESET)
+    minecraft_process.stdin.write(f"say {player} died, the server will restart with a new world.\n")
+    minecraft_process.stdin.flush()
+    time.sleep(1)
+    print(RED + f"[-]\t{deaths}" + RESET)
+    minecraft_process.stdin.write(f"say Here are the stats: {deaths}\n")
+    minecraft_process.stdin.flush()
+    time.sleep(5)
+    return
+
+
+def line_contains_two_users(line):
+    user_line_list = []
+    for user in usernames:
+        if user in line:
+            user_line_list.append(user)
+    
+    if len(user_line_list) > 1:
+        return True
+    else:
+        return False
+            
 
 def check_player_death():
         while True:
@@ -131,32 +202,57 @@ def check_player_death():
             out = line.replace("\n", "")
             print(YELLOW + out + RESET)
             if line.strip():
-                if any(keyword in line for keyword in death_messages):
-                    if not any(f"<{username}>" in line for username in usernames) \
-                        and not any(death_entities in line for death_entities in allowed_death_entities):
-                        for user in usernames:
-                            if line.__contains__(user) == True:
-                                dead_player = user
-                            else:
-                                continue
 
-                        print(RED + "[-]\tA player died" + RESET)
-                        minecraft_process.stdin.write(f"say {dead_player} died, the server will restart with a new world.\n")
-                        minecraft_process.stdin.flush()
-                        time.sleep(10)
-                        return
+                if any(username in line for username in usernames):
                     
+                    if any(keyword in line for keyword in death_messages):
+                        
+                        if not any(f"<{username}>" in line for username in usernames) \
+                            and not any(death_entities in line for death_entities in allowed_death_entities):
+                            
+                            for user in usernames:                         
+                                if line.__contains__(user) == True:
+                                    if line_contains_two_users(line) == True:
+                                        line = line.split(" was")
+                                        line = line[0].split(": ")
+                                        dead_player = line[1]
+                                    else:
+                                        dead_player = user
+
+                                    if dead_player in deaths:
+                                        deaths[f"{dead_player}"] += 1
+                                        print(RED + f"\n\n[!]\t{dead_player} got added a death to their counter" + RESET)
+                                    else:
+                                        deaths[f"{dead_player}"] = 1
+                                    player_died(dead_player)
+                                    return
+
+                                else:
+                                    continue
+                    
+                    elif "stats" in line.lower():
+                        print(GREEN + "[i]\tuser asked for stats, sending them" + RESET)
+                        minecraft_process.stdin.write(f"say Attempt {attempt_number} with the following deaths: {str(deaths).replace('{','').replace('}','')}\n")
+                        minecraft_process.stdin.flush()
+
                 elif " logged in with entity id" in line:
                     username = line.split("[")[2].split(": ")[1]
                     if username not in usernames:
                         usernames.append(username)
-                        print(BLUE + f"[!]\tUsername: {username} appended to list." + RESET)
-
+                        print(BLUE + f"[!]\tUsername: {username} appended to internal list." + RESET)
+             
+                                          
 ####    START   ####
 
-# Check for the file first:
-time.sleep(2)
 print_banner()
+usernames = []
+print(BLUE + "[i]\tReading stats from file." + RESET)
+deaths = read_stats_from_file()
+attempt_number = sum_of_deaths(deaths)
+print(GREEN + f"[i]\tThis is attempt number {attempt_number}."+ RESET)
+time.sleep(2)
+
+# Check for the file first:
 check_and_create_run_file(operating_system)
 
 if check_eula_agreement() == False:
@@ -218,4 +314,6 @@ while True:
     # Delete the world directory
     os.system(delete_command)
     print(RED + "[!]\tDirectory 'world' deleted." + RESET)
+    save_stats_to_file(deaths)
+    print(RED + f"[i]\tSaved all the stats to file." + RESET)
     time.sleep(1)
